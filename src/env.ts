08f61d661f58da32050f234b50eee6c5fde9215f
Marvin Fuchs <info@marvin-fuchs.de>
2025-04-24 10:59:20 +0200

    feat: Add basic backup with borgbackup

--- src/env.ts
+++ src/env.ts
@@ -4,9 +4,14 @@ import { z } from 'zod';
 const envSchema = z.object({
   RCLONE_CONF_FILE: z.string().default('/config/rclone.conf'),
   RCLONE_REMOTE: z.string(),
+
   TZ: z.string().default('UTC'),
   CRON_SCHEDULE: z.string().default('0 0 * * *'),
-  BACKUP_BASE_DIR: z.string().default('/data'),
+
+  BACKUP_DATA: z.string().default('/data'),
+  BACKUP_REPOSITORY: z.string().default('/repo'),
+  BACKUP_REPOSITORY_PASSWORD: z.string().optional(),
+
   POSTGRES_USER: z.string(),
 });
 
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,37 @@
 import { CronJob } from 'cron';
 import env from './env';
+import { create_postgres_backup } from './postgres';
+import { execa } from 'execa';
 
-const backup = () => {
-  console.log('Starting backup...');
+const backup = async () => {
+  console.info('Starting backup...');
+  console.info('Checking repository...');
+  const repoExists = await new Promise((resolve) => {
+    execa`borg info ${env.BACKUP_REPOSITORY}`
+      .then(() => resolve(true))
+      .catch(() => resolve(false));
+  });
+
+  if (!repoExists) {
+    console.log('Creating Borg repository...');
+    await execa({
+      env: {
+        BORG_PASSPHRASE: env.BACKUP_REPOSITORY_PASSWORD,
+      },
+    })`borg init --encryption=repokey ${env.BACKUP_REPOSITORY}`;
+  }
+
+  console.info('Creating postgres backup...');
+  await create_postgres_backup();
+
+  console.info('Backing up data...');
+  await execa({
+    env: {
+      BORG_PASSPHRASE: env.BACKUP_REPOSITORY_PASSWORD,
+    },
+    stdout: 'inherit',
+    stderr: 'inherit',
+  })`borg create --progress --stats --compression lz4 ${env.BACKUP_REPOSITORY}::'{now:%Y-%m-%d_%H:%M:%S}' ${env.BACKUP_DATA}`;
 };
 
 CronJob.from({
